658181bf7b54cce114aef633a035028ef410f546
abyanji <a.byanjil99@gmail.com>
2022-12-03 18:57:55 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+int firstDigit = int.Parse(Console.ReadLine());
+int secondDigit = int.Parse(Console.ReadLine());
+
+int result = secondDigit % firstDigit;
+if (result == 0)
+{
+    Console.WriteLine($"{secondDigit}, {firstDigit} -> кратно");
+}
+else
+{
+    Console.WriteLine($"{secondDigit}, {firstDigit} -> не кратно, остаток {result}");
+}
