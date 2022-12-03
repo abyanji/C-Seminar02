46e2d51d36a378e102e4a5abe46b11f8d8cb31dd
abyanji <a.byanjil99@gmail.com>
2022-12-03 19:13:27 +0300

    modify Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,14 +1,27 @@
-﻿Console.Clear();
-Console.WriteLine("Введите два числа");
-int firstDigit = int.Parse(Console.ReadLine());
-int secondDigit = int.Parse(Console.ReadLine());
+﻿// Console.Clear();
+// Console.WriteLine("Введите два числа");
+// int firstDigit = int.Parse(Console.ReadLine());
+// int secondDigit = int.Parse(Console.ReadLine());
 
-int result = secondDigit % firstDigit;
-if (result == 0)
+// int result = secondDigit % firstDigit;
+// if (result == 0)
+// {
+//     Console.WriteLine($"{secondDigit}, {firstDigit} -> кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"{secondDigit}, {firstDigit} -> не кратно, остаток {result}");
+// }
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int digit = int.Parse(Console.ReadLine());
+
+if (digit % 7 == 0 && digit % 23 == 0)
 {
-    Console.WriteLine($"{secondDigit}, {firstDigit} -> кратно");
+    Console.WriteLine($"{digit} -> да");
 }
 else
 {
-    Console.WriteLine($"{secondDigit}, {firstDigit} -> не кратно, остаток {result}");
+    Console.WriteLine($"{digit} -> нет");
 }
