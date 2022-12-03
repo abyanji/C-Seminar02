aeb250078484e72ffba602e877ac33c0b8d496de
abyanji <a.byanjil99@gmail.com>
2022-12-03 19:30:52 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+int firstDigit = int.Parse(Console.ReadLine());
+int secondDigit = int.Parse(Console.ReadLine());
+
+if (firstDigit / secondDigit == secondDigit || secondDigit / firstDigit == firstDigit)
+{
+    Console.WriteLine($"{firstDigit}, {secondDigit} -> да");
+}
+else
+{
+    Console.WriteLine($"{firstDigit}, {secondDigit} -> нет");
+}
