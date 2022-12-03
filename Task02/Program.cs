40dfd425f13687492266598c5023ee58f3fb9419
abyanji <a.byanjil99@gmail.com>
2022-12-03 15:38:55 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int randomDigit = new Random().Next(100, 1000);
+Console.WriteLine($"Слуачйное число {randomDigit}");
+
+int firstDigit = randomDigit / 100 * 10;
+
+int secondDigit = randomDigit % 10;
+
+int newDigit = firstDigit + secondDigit;
+Console.WriteLine($"Новое число {newDigit}");
