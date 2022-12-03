ce17143f0b03d3580cbbc97857eb3d68eb01aeaf
abyanji <a.byanjil99@gmail.com>
2022-12-03 15:18:45 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int randomDigit = new Random().Next(10, 100);
+Console.WriteLine($"Слуачйное число {randomDigit}");
+
+int remainder = randomDigit % 10;
+Console.WriteLine($"Последняя цифра {remainder}");
+
+int div = randomDigit / 10;
+Console.WriteLine($"Первая цифра {div}");
+
+int max = div;
+
+if (max < remainder)
+{
+    max = remainder;
+}
+Console.WriteLine($"Максимум {max}");
+
